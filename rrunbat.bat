10430727234c6600c3856321878deff42b3a303e
Bach Minh Quang <0947556012abc@gmail.com>
2026-05-25 18:53:09 +0700

    Add local run batch script

---
+++ rrunbat.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal
+
+cd /d "%~dp0"
+
+where go >nul 2>nul
+if errorlevel 1 (
+    echo Go is not installed or not available in PATH.
+    exit /b 1
+)
+
+if "%XROUTER_ADDR%"=="" set "XROUTER_ADDR=:1213"
+
+echo Starting XRouter on %XROUTER_ADDR%
+go run ./cmd/xrouter
